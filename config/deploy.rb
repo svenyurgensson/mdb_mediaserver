441dee7f60eb114689e283650ebb142210fb260c
Yury Batenko <jurbat@gmail.com>
2013-06-28 17:28:44 +0800

    Cleanap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,36 +1,8 @@
-
-# Optional settings:
 set :user, 'pubapi'    # Username in the server to SSH to.
-#   set :port, '30000'     # SSH port number.
-
 set :project_name, 'mediaserver'
-
-# Basic settings:
-#   domain       - The hostname to SSH to.
-#   deploy_to    - Path to deploy into.
-#   repository   - Git repo to clone from. (needed by mina/git)
-#   branch       - Branch name to deploy. (needed by mina/git)
-
 set :domain,      '54.235.213.159'
 set :deploy_to,   "/home/#{user}/#{project_name}"
-set :repository,  "ssh://git@develop.earthtv.com:7999/ASSET/mserv.git"
-set :branch,      'master'
 
-# Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
-# They will be linked in the 'deploy:link_shared_paths' step.
-#set :shared_paths, ['config/database.yml', 'log']
-
-
-# This task is the environment that is loaded for most commands, such as
-# `mina deploy` or `mina rake`.
-task :environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .rbenv-version to your repository.
-  # invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use[ruby-1.9.3-p125@default]'
-end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
 # For Rails apps, we'll make some of the shared paths that are shared between
